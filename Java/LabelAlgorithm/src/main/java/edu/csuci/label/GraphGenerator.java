b8d992c03019c330d42e1082b0356c4632e94cd3
Joel Helling <joel.helling904@myci.csuci.edu>
2016-10-03 22:11:37 -0700

    Replaced with chained append calls instead of regular concatenation.

--- Java/LabelAlgorithm/src/main/java/edu/csuci/label/GraphGenerator.java
+++ Java/LabelAlgorithm/src/main/java/edu/csuci/label/GraphGenerator.java
@@ -49,9 +49,9 @@ public class GraphGenerator {
         res.append("Graph:\n");
         int line = 0;
         for (Set<Integer> neighbors : graph) {
-            res.append((line++) + ": [");
+            res.append(line++).append(": [");
             for (Integer i : neighbors) {
-                res.append(i.toString() + ", ");
+                res.append(i.toString()).append(", ");
             }
             if (neighbors.size() > 0) {
                 res.delete(res.length() - 2, res.length());
